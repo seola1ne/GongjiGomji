8758af4ed9efc4ab3a5d83912754d4c34c79e113
seola1ne <seolalny@gmail.com>
2023-06-26 01:49:14 +0900

    ADD :: Firebase 연동

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { firestore } from './firebase';
 import Header from '../src/components/common/Header/Header';
 import Main from '../src/Pages/Main/Main';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './Pages/Create/Create';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log(firestore);
+  })
+
+  // create라는 변수로 firestore의 collection인 create에 접근
+  const create = firestore.collection("create");
+
+  // collection의 document인 "create_item"을 가져온다.
+  create.doc("create_item").get().then((doc) => {
+    // document의 데이터를 가져옴
+    console.log(doc.data());
+
+    // document의 id를 가져옴
+    console.log(doc.id);
+  })
+  
   return (
     <div className="App">
       <Header />
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+//firebase.js
+import firebase from "firebase/compat/app"
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    // firebase 설정과 관련된 개인 정보
+    apiKey: "AIzaSyBovbsO72uAR6Kxe0NJUqQTq5-7YTAEBGU",
+    authDomain: "gongjigomji.firebaseapp.com",
+    projectId: "gongjigomji",
+    storageBucket: "gongjigomji.appspot.com",
+    messagingSenderId: "1036636812266",
+    appId: "1:1036636812266:web:a41e09414fd082e54f3893",
+    measurementId: "G-W5Y8VKKZ96"
+};
+
+// firebaseConfig 정보로 firebase 시작
+firebase.initializeApp(firebaseConfig);
+
+// firebase의 firestore 인스턴스를 변수에 저장
+const firestore = firebase.firestore();
+
+// 필요한 곳에서 사용할 수 있도록 내보내기
+export { firestore };
